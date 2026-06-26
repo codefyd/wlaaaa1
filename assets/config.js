fffc0374162b0c6bcc987777156aa67a777e89df
codefyd <ahsnsy77@gmail.com>
2026-06-26 17:26:56 +0300

    Create config.js

---
+++ assets/config.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://ensbgfysabhydgywswyo.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_1MvOeZyb2tb36LT05Ed6Ng_6t0lIltg",
+
+  APP_NAME: "نظام ولاء الكافيهات",
+};
